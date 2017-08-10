9b5970e7a30829fc446239a55534198956ed0536
Camilo Salazar <camilo.salazar@cern.ch>
2017-08-10 17:06:03 +0200

    Changed to run over 100 events

--- Dracarys/python/ConfFile_cfg.py
+++ Dracarys/python/ConfFile_cfg.py
@@ -4,7 +4,7 @@ process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
