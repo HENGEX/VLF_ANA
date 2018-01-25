814cb9c0b323f21090d1f0edb9c11f85279adfac
Omar Zapata <Omar.Zapata@cern.ch>
2018-01-25 15:04:45 -0500

    modified paths for 2017 data

--- Dracarys/test/Submit_WJetsToLNu_HT.py
+++ Dracarys/test/Submit_WJetsToLNu_HT.py
@@ -1,6 +1,8 @@
 import commands as cmd
 import time
 
+'''
+NOTE: data from 2016 (backward support)
 ListOfSamples=['/WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM',
                '/WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM',
                '/WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/MINIAODSIM',
@@ -19,6 +21,36 @@ ListOfSamples=['/WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/R
                '/WJetsToLNu_HT-800To1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM',
                '/WJetsToLNu_HT-800To1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
 ]
+'''
+
+
+''' 
+data for 2017
+taking it from https://cmsweb.cern.ch/das/request?view=list&limit=50&instance=prod%2Fglobal&input=dataset++dataset%3D%2FWJetsToLNu_HT-*%2F*%2F*+release%3DCMSSW_9_4_*
+query: dataset  dataset=/WJetsToLNu_HT-*/*/* release=CMSSW_9_4_*
+output:
+Dataset: /WJetsToLNu_HT-70To100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall17NanoAOD-PUMoriond17_94X_mcRun2_asymptotic_v2-v1/NANOAODSIM
+Creation time: 2018-01-17 02:13:50, Physics group: NoGroup, Release: CMSSW_9_4_2, Status: VALID, Tag: 94X_mcRun2_asymptotic_v2, Type: mc
+Release, Blocks, Files, Runs, Configs, Parents, Children, Sites, Physics Groups, py , Subscribe to PhEDEx , XsecDB Sources: dbs3 show 
+Dataset: /WJetsToLNu_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall17NanoAOD-PUMoriond17_94X_mcRun2_asymptotic_v2-v1/NANOAODSIM
+Creation time: 2018-01-17 23:58:43, Physics group: NoGroup, Release: CMSSW_9_4_2, Status: VALID, Tag: 94X_mcRun2_asymptotic_v2, Type: mc
+Release, Blocks, Files, Runs, Configs, Parents, Children, Sites, Physics Groups, py , Subscribe to PhEDEx , XsecDB Sources: dbs3 show 
+Dataset: /WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall17NanoAOD-PUMoriond17_94X_mcRun2_asymptotic_v2_ext2-v1/NANOAODSIM
+Creation time: 2018-01-16 03:41:01, Physics group: NoGroup, Release: CMSSW_9_4_2, Status: VALID, Tag: 94X_mcRun2_asymptotic_v2, Type: mc
+Release, Blocks, Files, Runs, Configs, Parents, Children, Sites, Physics Groups, py , Subscribe to PhEDEx , XsecDB Sources: dbs3 show 
+Dataset: /WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall17NanoAOD-PUMoriond17_94X_mcRun2_asymptotic_v2_ext1-v1/NANOAODSIM
+Creation time: 2018-01-16 23:45:13, Physics group: NoGroup, Release: CMSSW_9_4_2, Status: VALID, Tag: 94X_mcRun2_asymptotic_v2, Type: mc
+Release, Blocks, Files, Runs, Configs, Parents, Children, Sites, Physics Groups, py , Subscribe to PhEDEx , XsecDB Sources: dbs3 show 
+Dataset: /WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall17NanoAOD-PUMoriond17_94X_mcRun2_asymptotic_v2-v1/NANOAODSIM
+Creation time: 2018-01-16 15:59:47, Physics group: NoGroup, Release: CMSSW_9_4_2, Status: VALID, Tag: 94X_mcRun2_asymptotic_v2, Type: mc
+Release, Blocks, Files, Runs, Configs, Parents, Children, Sites, Physics Groups, py , Subscribe to PhEDEx , XsecDB Sources: dbs3 show 
+
+'''
+ListOfSamples=['/WJetsToLNu_HT-70To100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall17NanoAOD-PUMoriond17_94X_mcRun2_asymptotic_v2-v1/NANOAODSIM',
+               '/WJetsToLNu_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall17NanoAOD-PUMoriond17_94X_mcRun2_asymptotic_v2-v1/NANOAODSIM',
+               '/WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall17NanoAOD-PUMoriond17_94X_mcRun2_asymptotic_v2_ext2-v1/NANOAODSIM',
+               '/WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall17NanoAOD-PUMoriond17_94X_mcRun2_asymptotic_v2_ext1-v1/NANOAODSIM',
+               '/WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall17NanoAOD-PUMoriond17_94X_mcRun2_asymptotic_v2-v1/NANOAODSIM']
 
 ITime=time.localtime()
 StringITime=str(ITime.tm_year)+'_'+str(ITime.tm_mon)+'_'+str(ITime.tm_mday)+'_'+str(ITime.tm_hour)+'_'+str(ITime.tm_min)
